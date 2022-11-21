903a4c857058938d70b3f9923389c2f2785c5d9d
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-11-21 23:43:30 +0000

    do not add prefix if it already exists

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,17 @@ export const transform = (pluginName: string, pluginPrefix: string) => {
         if (parts[0].startsWith('@')) {
             return `${parts[0]}/${pluginPrefix}`;
         } else {
-            return `${pluginPrefix}-${parts[0]}`;
+            if (parts[0].startsWith(`${pluginPrefix}-`)) {
+                return parts[0];
+            } else {
+                return `${pluginPrefix}-${parts[0]}`;
+            }
         }
     } else {
-        const last = parts.splice(parts.length - 1, 1);
+        const last = parts.splice(parts.length - 1, 1)[0];
+        if (last.startsWith(`${pluginPrefix}-`)) {
+            return `${parts.join('/')}/${last}`;
+        }
         return `${parts.join('/')}/${pluginPrefix}-${last}`;
     }
 }
